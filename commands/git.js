5a20875a75171bf3df71b119c6ff962fe2c8fab2
arynapril <arynapril@gmail.com>
2017-07-18 11:12:36 -0400

    minor changes

--- commands/git.js
+++ commands/git.js
@@ -1,5 +1,5 @@
 exports.run = (bot, message, args, level) => {
-    message.channel.send("Here is my github repository! https://github.com/arynapril/arynbot :heart:").catch(console.error);
+    message.channel.send("Here is my github repository! https://github.com/arynapril/gaybot :heart:").catch(console.error);
 }
 
 exports.conf = {
@@ -11,6 +11,6 @@ exports.conf = {
 
 exports.help = {
     name: 'git',
-    description: 'Provides a link to arynbots github repository!',
+    description: 'Provides a link to gaybots github repository!',
     usage: 'git'
 };
