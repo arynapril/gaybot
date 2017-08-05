b59e3561a5fc5eb1a610c7786077338981552858
arynapril <arynapril@gmail.com>
2017-08-05 15:08:19 -0400

    for rn, giveme inputs are case sensitive, will fix

--- commands/giveme.js
+++ commands/giveme.js
@@ -1,5 +1,5 @@
 exports.run = async(bot, message, args, level) => {
-    const allowedRoles = ['social', 'Alumni', 'Mentor', 'volunteer', 'FTC', 'vex', 'ally', 'male', 'female', 'agender', 'genderfluid', 'demiboy', 'demigirl', 'nonbinary', 'gay', 'lesbian', 'bisexual', 'asexual', 'pansexual', 'straight', 'demisexual', 'polyamorous', 'questioning', 'queer'];
+    const allowedRoles = ['social', 'Alumni', 'Mentor', 'Volunteer', 'FTC', 'VEX', 'ally', 'male', 'female', 'agender', 'genderfluid', 'demiboy', 'demigirl', 'nonbinary', 'gay', 'lesbian', 'bisexual', 'asexual', 'pansexual', 'straight', 'demisexual', 'polyamorous', 'questioning', 'queer'];
     var roleToGive = args;
     var found = false;
     var role = null;
