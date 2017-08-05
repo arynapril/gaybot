732baeea445282fc63d8819b34c79741e2faac8e
arynapril <arynapril@gmail.com>
2017-08-05 12:04:50 -0400

    minor update to welcome message

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,6 +2,6 @@ module.exports = (bot, member) => {
     if (member.guild.id !== bot.config.guildID) return;
     var screening = member.guild.channels.find('name', 'screening-lobby');
     var rules = member.guild.channels.find('name', 'server-rules');
-    screening.send(`Hi ${member}! Welcome to LGBTQ+ of FIRST! Please read through the rules (found in ${rules}) for instructions on how to get access to the rest of the server!`);
+    screening.send(`Hi ${member}! Welcome to LGBTQ+ of FIRST! Please read through the rules (found in ${rules}) and then type **I have read the rules and regulations** to gain access! Thank you!`);
     bot.log("log", `Member joined - ${member.user.username} (${member.id})`);
 };
