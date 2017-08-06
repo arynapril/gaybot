c0bd3808017ed5f4703da97cd3b056c9c4e7d9e4
arynapril <arynapril@gmail.com>
2017-08-06 12:30:06 -0400

    Give doesnt ping twice anymore

--- commands/give.js
+++ commands/give.js
@@ -8,9 +8,9 @@ exports.run = (bot, message, args, level) => {
     } else if (role.comparePositionTo(message.member.highestRole) < 0) {
         message.guild.members.get(user.id).addRole(role).then(m => {
             if(m.roles.has(role.id))
-                message.channel.send(`Successfully gave ${user} the ${roleToGive} role!`);
+                message.channel.send(`Successfully gave **${user.username}** the **${roleToGive}** role!`);
             else
-                message.channel.send(`Could not give ${user} the ${roleToGive} role! Sorry!`);
+                message.channel.send(`Could not give **${user.username}** the **${roleToGive}** role! Sorry!`);
         }).catch(console.error);
     } else
         message.channel.send("Your highest role is lower than this role, so you cannot give it! Sorry!");
