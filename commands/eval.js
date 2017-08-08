209fde1221bcae595a1bcfa5bdea513f3d5eb9f4
arynapril <arynapril@gmail.com>
2017-08-08 01:36:01 -0400

    prettier eval

--- commands/eval.js
+++ commands/eval.js
@@ -1,13 +1,21 @@
 const Discord = require("discord.js");
 exports.run = async(bot, message, args, level) => {
     const code = args.join(" ");
+    var evalEmbed = new Discord.RichEmbed()
+        .setTitle('Evaluated code')
+        .setTimestamp()
+        .setFooter('EVAL')
+        .addField('Input', code)
     try {
         const evaled = eval(code);
         const clean = await bot.clean(bot, evaled);
-        message.channel.send(`\`\`\`xl\n${clean}\n\`\`\``);
+        evalEmbed.setColor([Math.floor(Math.random()*256),255,Math.floor(Math.random()*256)])
+            .addField('OUTPUT', clean)
     } catch (err) {
-        message.channel.send(`\`ERROR\` \`\`\`xl\n${await bot.clean(bot, err)}\n\`\`\``);
+        evalEmbed.setColor([255,Math.floor(Math.random()*256),Math.floor(Math.random()*256)])
+            .addField('ERROR', await bot.clean(bot, err))
     }
+    message.channel.send({embed: evalEmbed});
 };
 
 exports.conf = {
