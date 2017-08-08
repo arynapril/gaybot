d51ad730067293c803e401b358db7eaf6ce8ed58
arynapril <arynapril@gmail.com>
2017-08-08 01:14:45 -0400

    some updates to giveme

--- commands/giveme.js
+++ commands/giveme.js
@@ -1,8 +1,15 @@
 exports.run = async(bot, message, args, level) => {
+    const Discord = require('discord.js')
+    var colors = Array("#FF80AB", "#FF4081", "#F50057", "#EF5350", "#E53935", "#C62828", "#FFAB40", "#FF9100", "#FF6D00", "#FFD740", "#FFC400", "#FFAB00", "#81C784", "#4CAF50", "#388E3C", "#2196F3", "#1976D2", "#0D47A1", "#7986CB", "#3F51B5", "#303F9F", "#B39DDB", "#7E57C2", "#5E35B1");
     const allowedRoles = ['social', 'Alumni', 'Mentor', 'Volunteer', 'FRC', 'FTC', 'VEX', 'FGC', 'FLL', 'ally', 'male', 'female', 'agender', 'genderfluid', 'demiboy', 'demigirl', 'nonbinary', 'gay', 'lesbian', 'bisexual', 'asexual', 'pansexual', 'straight', 'demisexual', 'polyamorous', 'questioning', 'queer'];
-    var found = false;
-    var role = null;
+    var assigned = "";
+    var assignedCount = 0;
+    var alreadyHad = "";
+    var alreadyHadCount = 0;
+    var notFoundCount = 0;
     for (var k = 0; k < args.length; k++) {
+        var found = false;
+        var role = null;
         var roleToGive = args[k].toLowerCase();
         for (var i = 0; i < allowedRoles.length; i++) {
             if (allowedRoles[i].toLowerCase() == roleToGive) {
@@ -13,10 +20,12 @@ exports.run = async(bot, message, args, level) => {
         if (found == true) {
             var member = message.member;
             if (member.roles.has(role.id)) {
-                message.channel.send("You already have that role!");
+                alreadyHad += `${role.name} \n`;
+                alreadyHadCount++;
             } else {
                 member.addRole(role).catch(console.error);
-                message.channel.send("Successfully gave you **" + role.name + "**!");
+                assigned += `${role.name} \n`;
+                assignedCount++;
             }
         } else if (roleToGive == "list" || roleToGive == "-l") {
             var list = "**List of Allowed Roles**";
@@ -24,8 +33,22 @@ exports.run = async(bot, message, args, level) => {
             list += "\n For the trans role or roles according to your region, ping or DM an admin!";
             message.channel.send(list);
         } else {
+            notFoundCount++;
         };
     };
+    var givemeEmbed = new Discord.RichEmbed()
+        .setTimestamp()
+        .setColor(colors[Math.floor(Math.random() * colors.length)]);
+    if (assignedCount>0) {
+        givemeEmbed.addField(`Successfully gave you ${assignedCount} role(s)!`, assigned);
+    };
+    if (alreadyHadCount>0) {
+        givemeEmbed.addField(`You already had ${alreadyHadCount} role(s)!`, alreadyHad);
+    };
+    if (notFoundCount>0) {
+        givemeEmbed.addField(`Couldn't find ${notFoundCount} role(s)!`, 'Use .giveme list to get a list of roles avaliable!');
+    };
+    message.channel.send({embed: givemeEmbed})
 };
 exports.conf = {
     enabled: true,
