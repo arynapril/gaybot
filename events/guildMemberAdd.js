2f0b808a3a13cacaaaa7f0c45a1ff1a438f51393
Aryn Thomas <arynapril@gmail.com>
2017-10-25 23:09:29 -0400

    updated her welcome messages

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,6 +2,6 @@ module.exports = (bot, member) => {
     if (member.guild.id !== bot.config.guildID) return;
     var screening = member.guild.channels.find('name', 'screening-lobby');
     var rules = member.guild.channels.find('name', 'server-rules');
-    screening.send(`Hi ${member}! Welcome to LGBTQ+ of FIRST! Please read through the rules (found in ${rules}) and then type **I have read the rules and regulations** to gain access! Thank you!`);
+    screening.send(`Hi ${member}! Welcome to LGBTQ+ of FIRST! Please read through the rules (found in ${rules}), set your nickname to include your pronouns and team number, and then type **I have read the rules and regulations** to gain access! Thank you!`);
     bot.log("log", `Member joined - ${member.user.username} (${member.id})`, 'MEMBR');
 };
