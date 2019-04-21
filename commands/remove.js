d05e6d137c1d82bcd653c460dde59bde46bbe481
Aryn Thomas <arynapril@gmail.com>
2019-04-21 18:21:16 -0400

    adding more roles to the giveme

--- commands/remove.js
+++ commands/remove.js
@@ -1,5 +1,5 @@
 exports.run = async(bot, message, args, level) => {
-    const allowedRoles = ['social', 'Alumni', 'Mentor', 'Volunteer', 'FRC', 'FTC', 'VEX', 'FGC', 'FLL', 'ally', 'aromantic', 'male', 'female', 'agender', 'genderfluid', 'demiboy', 'demigirl', 'nonbinary', 'transsexual', 'genderqueer', 'gay', 'lesbian', 'bisexual', 'asexual', 'pansexual', 'straight', 'demisexual', 'questioning', 'queer', 'polysexual'];
+    const allowedRoles = ['social', 'Alumni', 'Mentor', 'Volunteer', 'FRC', 'FTC', 'VEX', 'FGC', 'FLL', 'ally', 'aromantic', 'male', 'female', 'agender', 'genderfluid', 'demiboy', 'demigirl', 'nonbinary', 'transsexual', 'genderqueer', 'gay', 'lesbian', 'bisexual', 'asexual', 'pansexual', 'straight', 'demisexual', 'questioning', 'queer', 'polysexual', 'gender-non-conforming', 'grayromantic', 'closeted', 'Houston', 'Detroit'];
     var found = false;
     var role = null;
     for (var k = 0; k < args.length; k++) {
