98c9e2fd028901694952f8a63acc53fc0cd83ec3
Aryn Thomas <arynapril@gmail.com>
2019-05-20 13:16:55 -0400

    more game updates

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,16 @@ module.exports = async bot => {
 	await wait(1000);
 	bot.syncServers();
 	bot.log("log", `Ready to love ${bot.users.size} users in ${bot.guilds.size} servers!`, "READY");
-	bot.user.setPresence({ game: { name: `❤️🧡💛💚💙💜` }, status: 'online' })
+	const list = [
+		"❤️🧡💛💚💙💜", 
+		"💙💖⚪💖💙",
+		"💖💜💙", 
+		"💖💛💙"
+		];
+	index = -1;
+	setInterval(() => {
+		index += 1;
+		if (index == list.length) index = 0; 
+		client.user.setPresence({ game: { name: list[index] }, status: 'online' }); // sets bot's activities to one of the phrases in the arraylist.
+	}, 30000);
 }
